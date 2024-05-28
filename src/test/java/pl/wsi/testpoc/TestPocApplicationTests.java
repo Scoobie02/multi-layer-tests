528a2e18367ca8ec55bad6899bba8f1de1364be9
wsi <skubala.wojciech@gmail.com>
2024-05-28 18:51:11 +0200

    init

---
+++ src/test/java/pl/wsi/testpoc/TestPocApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wsi.testpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
